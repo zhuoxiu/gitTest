4429f59e9924dfa622c73dc61c31b42a2bb53b70
zhuoxiu <3107711907@qq.com>
2020-04-09 00:23:56 +0800

    给代码添加了注释

--- main/java/com/zhuoxiu/git_test.java
+++ main/java/com/zhuoxiu/git_test.java
@@ -1,11 +1,16 @@
 package com.zhuoxiu;
 
+
+/**
+ * git上传到github测试代码
+ */
 public class git_test {
     public static void main(String[] args) {
         forMethod();
     }
 
     private static void forMethod(){
+        //循环打印10次
         for (int i = 0; i < 10; i++) {
             System.out.println("hello world");
         }
