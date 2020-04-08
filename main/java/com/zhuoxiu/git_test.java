155a38b822fdaf4d88af4e27a498a67f02ab6be0
zhuoxiu <3107711907@qq.com>
2020-04-09 00:11:53 +0800

    第一次提交到本地仓库

---
+++ main/java/com/zhuoxiu/git_test.java
@@ -0,0 +1,13 @@
+package com.zhuoxiu;
+
+public class git_test {
+    public static void main(String[] args) {
+        forMethod();
+    }
+
+    private static void forMethod(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world");
+        }
+    }
+}
